7c293afa88ed9689f4f1a1ec914e2eb4eef29328
Alfredo García Suárez <110580458+freddy-7@users.noreply.github.com>
2026-04-22 18:11:36 -0600

    Add files via upload

---
+++ Solucion_examen_circunferencia.m
@@ -0,0 +1,150 @@
+clear
+close all
+clc
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TIEMPO %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+tf = 16;             % Tiempo de simulacion (ajustado al dominio)
+ts = 0.01;
+t = 0:ts:tf;
+
+N = length(t);       
+
+
+%%%%%%%%%%%%%%%%%%%%%%%% CONDICIONES INICIALES %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+x1 = zeros(1,N+1);  
+y1 = zeros(1,N+1);  
+phi = zeros(1,N+1); 
+
+% Condiciones de la trayectoria específicas
+x1(1) = -4;              
+y1(1) = 0;           
+phi(1) = pi/2;     
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% PUNTO DE CONTROL %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+hx = zeros(1,N+1);  
+hy = zeros(1,N+1);  
+
+hx(1) = x1(1);
+hy(1) = y1(1);
+
+%%%%%%%%%%%%%%%%%%%%%% VELOCIDADES DE REFERENCIA %%%%%%%%%%%%%%%%%%%%%%%%%%
+
+u = zeros(1,N); % velocidad lineal
+w = zeros(1,N); % velocidad angular
+
+for k = 1:N
+    %x=t
+    %y=x2+y2−16=0
+    %y=±((16 - x^2)^1/2);
+    %y=±((16 - t^2)^1/2);
+    %t= [-4 a 4]
+    %θ(𝒕) = t𝒂𝒏^−𝟏(t / sqrt(16 - t^2))
+  
+    tk = t(k);
+
+   % -------- PARTE SUPERIOR --------
+    if tk <= 7.999
+    tNorm = -3.999 + tk;        % tiempo normalizado arriba [-4 a 4]
+
+    dx = 1;
+    dy = -(tNorm) / sqrt(16 - (tNorm)^2);
+    
+    % Velocidad lineal
+    u(k) = sqrt(dx^2 + dy^2);
+    
+    % Velocidad angular
+    w(k) = (-16 / (16 - (tNorm)^2)^(3/2)) / (1 + ((tNorm) / sqrt(16 - (tNorm)^2))^2);
+
+        
+    % -------- PARTE INFERIOR --------
+    else
+        tNorm = 3.999 - (tk - 7.999);   % tiempo normalizado abajo [4 a -4]
+
+        dx = 1;
+        dy = -tNorm/ sqrt(16 - (tNorm)^2);   % CAMBIA SIGNO
+        
+        u(k) = sqrt(dx^2 + dy^2);
+        
+        w(k) = (-16 / (16 - (tNorm)^2)^(3/2)) / (1 + (tNorm / sqrt(16 - (tNorm)^2))^2);
+        
+    end
+    
+end
+    
+
+%%%%%%%%%%%%%%%%%%%%%%%%% BUCLE DE SIMULACION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+for k=1:N 
+    
+    phi(k+1)=phi(k)+w(k)*ts; % Integral numérica (método de Euler)
+    
+    %%%%%%%%%%%%%%%%%%%%% MODELO CINEMATICO %%%%%%%%%%%%%%%%%%%%%%%%%
+    %Aplicamos el modelo cinemático diferencial para obtener las
+    %velocidades en x, y, phi
+    xp1=u(k)*cos(phi(k+1)); 
+    yp1=u(k)*sin(phi(k+1));
+    phip= w(k)
+
+    x1(k+1)=x1(k) + xp1*ts ; % Integral numérica (método de Euler)
+    y1(k+1)=y1(k) + yp1*ts ; % Integral numérica (método de Euler)
+    
+    
+    % Posicion del robot con respecto al punto de control
+    hx(k+1)=x1(k+1); 
+    hy(k+1)=y1(k+1);
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% SIMULACION VIRTUAL 3D %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% a) Configuracion de escena
+
+scene=figure;  % Crear figura (Escena)
+set(scene,'Color','white'); % Color del fondo de la escena
+set(gca,'FontWeight','bold') ;% Negrilla en los ejes y etiquetas
+sizeScreen=get(0,'ScreenSize'); % Retorna el tamaño de la pantalla del computador
+set(scene,'position',sizeScreen); % Congigurar tamaño de la figura
+camlight('headlight'); % Luz para la escena
+axis equal; % Establece la relación de aspecto para que las unidades de datos sean las mismas en todas las direcciones.
+grid on; % Mostrar líneas de cuadrícula en los ejes
+box on; % Mostrar contorno de ejes
+xlabel('x(m)'); ylabel('y(m)'); zlabel('z(m)'); % Etiqueta de los eje
+
+view([5 40]); % Orientacion de la figura
+axis([-5 5 -5 5 0 4]); % Ingresar limites minimos y maximos en los ejes x y z [minX maxX minY maxY minZ maxZ]
+
+% b) Graficar robots en la posicion inicial
+scale = 4;
+MobileRobot_5;
+H1=MobilePlot_4(x1(1),y1(1),phi(1),scale);hold on;
+
+% c) Graficar Trayectorias
+H2=plot3(hx(1),hy(1),0,'r','lineWidth',2);
+
+% d) Bucle de simulacion de movimiento del robot
+
+step=1; % pasos para simulacion
+
+for k=1:step:N
+
+    delete(H1);    
+    delete(H2);
+    
+    H1=MobilePlot_4(x1(k),y1(k),phi(k),scale);
+    H2=plot3(hx(1:k),hy(1:k),zeros(1,k),'r','lineWidth',2);
+    
+    pause(ts);
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Graficas %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+graph=figure;  % Crear figura (Escena)
+set(graph,'position',sizeScreen); % Congigurar tamaño de la figura
+subplot(211)
+plot(t,u,'b','LineWidth',2),grid('on'),xlabel('Tiempo [s]'),ylabel('m/s'),legend('u');
+subplot(212)
+plot(t,w,'r','LineWidth',2),grid('on'),xlabel('Tiempo [s]'),ylabel('[rad/s]'),legend('w');
+
+
